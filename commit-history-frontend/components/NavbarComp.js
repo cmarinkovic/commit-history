43b1c6a2724f812935d8e66677b547c29f30263f
Cristóbal Marinkovic <cristobal@comoquiero.cl>
2022-10-17 20:30:33 -0300

    Added Navbar component

--- commit-history-frontend/components/NavbarComp.js
+++ commit-history-frontend/components/NavbarComp.js
@@ -1,3 +1,32 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+import { Navbar } from "flowbite-react";
+
 export default function NavbarComp() {
-  return <div></div>;
+  const router = useRouter();
+
+  return (
+    <Navbar fluid={true} rounded={true}>
+      <Navbar.Brand href="https://flowbite.com/">
+        <img
+          src="/logo.png"
+          className="mr-3 h-6 sm:h-9"
+          alt="Commit History Logo"
+        />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+          Commit History
+        </span>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse>
+        <Navbar.Link active={router.pathname === "/"}>
+          <Link href="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={router.pathname === "/about"}>
+          <Link href="about">About </Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
