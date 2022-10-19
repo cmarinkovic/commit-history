b40aa0a207fc5c8cffa25777d6947b2b8ac81b54
Cristóbal Marinkovic <cristobal@comoquiero.cl>
2022-10-19 02:27:15 -0300

    Improved readability for ErrorAlert component

--- commit-history-frontend/components/ErrorAlert.js
+++ commit-history-frontend/components/ErrorAlert.js
@@ -2,7 +2,7 @@ import { Alert } from "flowbite-react";
 
 export default function ErrorAlert({ errorMessage }) {
   return (
-    <div style={{ display: !errorMessage ? "none" : "block" }}>
+    <div style={{ display: errorMessage ? "block" : "none" }}>
       <Alert color="failure" rounded={true}>
         <span className="font-medium">{errorMessage}</span>
       </Alert>
