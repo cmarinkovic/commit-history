f0cd6061ff414994ff3b6488d746532be4c655ac
Cristóbal Marinkovic <cristobal@comoquiero.cl>
2022-10-18 22:23:28 -0300

    Updated controller tests

--- commit-history-backend/src/commits/commits.controller.spec.ts
+++ commit-history-backend/src/commits/commits.controller.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { CommitsController } from './commits.controller';
+import { CommitsModule } from './commits.module';
+import { CommitsService } from './commits.service';
 
 describe('CommitsController', () => {
   let controller: CommitsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [CommitsModule],
       controllers: [CommitsController],
+      providers: [CommitsService],
     }).compile();
 
     controller = module.get<CommitsController>(CommitsController);
