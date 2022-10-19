6394c8be1c8677ffbd85d2fa4a815d423529aeb6
Cristóbal Marinkovic <cristobal@comoquiero.cl>
2022-10-19 00:04:13 -0300

    Added stateful fields for performing request

--- commit-history-frontend/components/CommitsTimeline.js
+++ commit-history-frontend/components/CommitsTimeline.js
@@ -1,16 +1,49 @@
-import { Timeline, Card } from "flowbite-react";
-import { CalendarIcon } from "@heroicons/react/24/solid";
+import { useState } from "react";
+import { Timeline, Card, TextInput, Button, Spinner } from "flowbite-react";
+import { CalendarIcon, ArrowPathIcon } from "@heroicons/react/24/solid";
 
 import DetailsAccordion from "./DetailsAccordion";
 
 export default function CommitsTimeline() {
+  const [repositoryName, setRepositoryName] = useState("");
+  const [repositoryOwner, setRepositoryOwner] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="max-w-screen-md min-w-min mx-auto">
       <div className="mx-8">
         <Card>
           <h5 className="text-2xl tracking-tight text-gray-900 dark:text-white">
-            <span className="font-medium ">demo-repo</span> by{" "}
-            <span className="font-medium ">demo-user</span>
+            <form className="grid grid-cols-12 gap-x-2">
+              <span className="col-span-3">
+                <TextInput
+                  value={repositoryName}
+                  className="font-medium"
+                  placeholder="Repository name"
+                  onChange={(e) => setRepositoryName(e.target.value)}
+                ></TextInput>
+              </span>
+              <span className="text-md place-self-center">by</span>
+              <span className="col-span-3">
+                <TextInput
+                  value={repositoryOwner}
+                  className="font-medium"
+                  placeholder="Repository owner"
+                  onChange={(e) => setRepositoryOwner(e.target.value)}
+                ></TextInput>
+              </span>
+              <Button type="submit" onClick={handleSubmit}>
+                {isLoading ? (
+                  <Spinner size="md" light={true} />
+                ) : (
+                  <ArrowPathIcon className="h-6 w-6" />
+                )}
+              </Button>
+            </form>
           </h5>
           <Timeline>
             <Timeline.Item>
