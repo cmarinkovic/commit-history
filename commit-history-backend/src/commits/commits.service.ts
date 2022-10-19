fc77ea21dc2a28b8bd39a5c41c8be10ac4611537
Cristóbal Marinkovic <cristobal@comoquiero.cl>
2022-10-18 22:27:51 -0300

    Added authentication methods

--- commit-history-backend/src/commits/commits.service.ts
+++ commit-history-backend/src/commits/commits.service.ts
@@ -1,4 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { Octokit } from 'octokit';
+
+import { OctokitResponse } from '@octokit/types';
 
 @Injectable()
 export class CommitsService {
@@ -8,4 +11,34 @@ export class CommitsService {
     username: '',
     repository: '',
   };
+
+  isAuthenticated(): { isAuthenticated: boolean; owner: string } {
+    if (!this.octokit) return { isAuthenticated: false, owner: '' };
+
+    return { isAuthenticated: true, owner: this.ownerData.username };
+  }
+
+  async authenticate(payload: {
+    personalToken: string;
+  }): Promise<{ owner: string }> {
+    const { personalToken } = payload;
+    const regex = new RegExp('^ghp_[A-Za-z0-9]{36}$');
+    if (!regex.test(personalToken)) {
+      throw new BadRequestException('Invalid token.');
+    }
+
+    const octokit = new Octokit({
+      auth: personalToken,
+    });
+    if (!octokit) throw new BadRequestException('Invalid token.');
+
+    const response: OctokitResponse<any> =
+      await octokit.rest.users.getAuthenticated();
+    if (!response) throw new BadRequestException('Invalid token.');
+
+    this.ownerData.username = response.data.login;
+    this.octokit = octokit;
+
+    return { owner: this.ownerData.username };
+  }
 }
